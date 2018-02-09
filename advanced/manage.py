cafc0ecae9e720207dea3d1228869527bd52044b
Samuel J. Lawson <samuel.lawson@angieslist.com>
2018-02-09 11:10:29 -0500

    keeping up appearances

--- advanced/manage.py
+++ advanced/manage.py
@@ -28,12 +28,32 @@ def testall():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
+    
 @manager.command
-def testgroup():
+def test_h1TitleCanonical():
+    print("Starting Python Flask Selenium Test Framework")
+    print("Visitor site: ", os.environ['VISITOR_SITE_URL'])
+    tests = unittest.TestLoader().discover('tests', pattern='test_h1TitleCanonical.py')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+    
+@manager.command
+def test_redirects():
     print("Starting Python Flask Selenium Test Framework")
     print("Visitor site: ", os.environ['VISITOR_SITE_URL'])
     tests = unittest.TestLoader().discover('tests', pattern='test_Redirects.py')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+# @manager.command
+# def sandbox():
+#     import argparse
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("command", nargs='+', help="command missing", type=str)
+#     parser.add_argument("test", nargs='+', help="[test identifier]", type=str)
+#     args = parser.parse_args()
+#     print(args.command)
+#     if args.test:
+#         print("Test to run %s", args.test_to_run)
     
 if __name__ == '__main__':
     manager.run()
