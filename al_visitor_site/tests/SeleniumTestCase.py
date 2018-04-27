05827c7bb12b6f6fd9e4e65dd38fc8b3988a16e3
Samuel J. Lawson <samuel.lawson@angieslist.com>
2018-04-27 15:13:25 -0400

    Experimenting with adding proxy and checking network calls

--- al_visitor_site/tests/SeleniumTestCase.py
+++ al_visitor_site/tests/SeleniumTestCase.py
@@ -2,14 +2,24 @@ import unittest, sys, datetime, time, os
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from browsermobproxy import Server
 
 class SeleniumTestCase(unittest.TestCase):
     client = None
+    server = None
+    proxy = None
     visitor_site_url = os.environ.get('VISITOR_SITE_URL', 'http://angiesmr2stg.prod.acquia-sites.com')
     browser_clients = os.environ.get('BROWSER_CLIENTS', 'Chrome').split(',')
     test_browser = int(os.environ.get('TEST_BROWSER', 0))
+    browsermob_path = os.environ.get('BROWSERMOB_PATH', './../browsermob-proxy/bin/browsermob-proxy')
+    browsermob_port = int(os.environ.get('BROWSERMOB_PORT', '9090'))
+    browsermob_host = os.environ.get('BROWSERMOB_HOST', '127.0.0.1')
     
     def setUp(self):
+        self.server = Server(self.browsermob_path, {'host':self.browsermob_host,'port':self.browsermob_port})
+        self.server.start()
+        self.proxy = self.server.create_proxy()
+        print(self.proxy.selenium_proxy().httpProxy)
         method_name = self.browser_clients[self.test_browser]
         try:
             client_method = getattr(webdriver, method_name)
@@ -20,10 +30,14 @@ class SeleniumTestCase(unittest.TestCase):
             d = getattr(DesiredCapabilities, method_name.upper())
             d['loggingPrefs'] = { 'browser':'ALL' }
             if method_name == 'Chrome':
-                browser = client_method(desired_capabilities=d)
+                ch_profile = webdriver.ChromeOptions()
+                ch_profile.add_argument('--proxy-server=http://%s' % self.proxy.selenium_proxy().httpProxy)
+                browser = client_method(desired_capabilities=d, chrome_options=ch_profile)
             elif method_name == 'Firefox':
                 fp = webdriver.FirefoxProfile()
+                fp.set_proxy(self.proxy.selenium_proxy())
                 browser = client_method(capabilities=d,firefox_profile=fp)
+                
             else:
                 browser = client_method()
             
@@ -31,12 +45,14 @@ class SeleniumTestCase(unittest.TestCase):
         except:
             print('Web browser not available')
             self.skiptest('Browser not available')
-
+        
         time.sleep(1)
 
     def tearDown(self):
         if self.client:
             self.client.close()
+        if self.server:
+            self.server.stop()
 
     def isElementPresent(self, cssSelector):
         try:
--- al_visitor_site/tests/test_JoinBtnSegment.py
+++ al_visitor_site/tests/test_JoinBtnSegment.py
@@ -5,6 +5,8 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.touch_actions import TouchActions
+from selenium.webdriver.common.action_chains import ActionChains
 
 # NOTE: Must select Chrome for this test.
 # currently only Chrome can test for console errors
@@ -13,12 +15,19 @@ class JoinBtnSegmentTestCase(SeleniumTestCase):
     def test_homePageJSError(self):
         wait = WebDriverWait(self.client, 20)
         if self.client:
+            # har load
+            self.proxy.new_har("visitor-site")
             self.client.get(self.visitor_site_url)
-            join_link = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#header-join')))
-            join_link.click()
+            # join_link = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#header-join')))
+            join_link = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#business-center')))
+            # har1 = self.proxy.har
+            # self.proxy.new_har("visitor-site-click")
+            actions = ActionChains(self.client).move_to_element(join_link).click().perform()
             console_logs = self.client.get_log('browser')
-            # grab log right after click, hopefully before redirect
-            wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#signup--signup-continue')))
+            # grab log right after click, before redirect
+            # wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#signup--signup-continue')))
+            wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#block-block-4')))
+            har = self.proxy.har
 
             severe_logs = []
             for console_log in console_logs:
@@ -28,10 +37,11 @@ class JoinBtnSegmentTestCase(SeleniumTestCase):
                 print("Console errors: ")
                 print(severe_logs)
             self.assertFalse(len(severe_logs))
+
             # to make sure you get the logs *before* the page load, add something like:
             # jQuery('#header-join').on('click', function() { console.log('CLICK EVENT: ' + document.location.href);});
             # print("Full logs:")
             # print(console_logs)
+            print(har)
         else:
             print("Client not available")
-            
