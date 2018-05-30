75929bed97e4e78911113513aab6e2c9db962d39
AaronBe <aaronbe@angieslist.com>
2018-05-30 17:29:58 -0400

    Hero Join CTA & New file name
    Changed the file name and added in a new test for the homePageSegmentHeroJoinCta.

--- al_visitor_site/tests/test_homePageSegment.py
+++ al_visitor_site/tests/test_homePageSegment.py
@@ -32,7 +32,7 @@ class JoinBtnSegmentTestCase(SeleniumTestCase):
             return 0
 
         # req'd params: current test case (self), relative path to triggering page, target element, and the triggering action
-        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/?asdgahg')
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/?CacheBuster')
 
         # describe what you are expecting in the segment call.
         # main field is how Python finds the test data in the page info, should probably be 'name' or 'activityLocation'
@@ -46,6 +46,33 @@ class JoinBtnSegmentTestCase(SeleniumTestCase):
                 ('title', 'Angie\'s List | Join for FREE to see 10 Million Verified Reviews'),
                 ('srCtaDisplayed', True),
                 ('srCtaVersion', 'v2.1'),
+                ('userId', ''),
+            ]
+        }
+
+        SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
+
+
+
+    # basic page call test
+    def test_homePageSegmentHeroJoinCta(self):
+        # quit if browser didn't load
+        if not self.client:
+            return 0
+
+        # req'd params: current test case (self), relative path to triggering page, target element, and the triggering action
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/', '#hero-join-cta-v2-1', 'click')
+
+        # describe what you are expecting in the segment call.
+        # main field is how Python finds the test data in the page info, should probably be 'name' or 'activityLocation'
+        # segment_params is a list of other segment fields to check. Can include main field as well.
+        # Take note of formatting...
+        segcall_info = {
+            'main_field': 'activityLocation',
+            'main_value': 'Visitor : Home',
+            'segment_params': [
+                ('description', 'Join link in hero image'),
+                ('userId', ''),
             ]
         }
 
