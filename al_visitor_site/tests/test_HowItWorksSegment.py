ab8a35365eb220c090da3165f304bb5d7155dcbd
AaronBe <aaronbe@angieslist.com>
2018-06-05 08:53:48 -0400

    adding to solution center and how it works

--- al_visitor_site/tests/test_HowItWorksSegment.py
+++ al_visitor_site/tests/test_HowItWorksSegment.py
@@ -1,22 +1,17 @@
 from tests.SeleniumTestCase import SeleniumTestCase
 from SegmentTestHelper import SegmentTestHelper
 
-
-# NOTE: Must select Chrome for this test.
-# TODO: adapt for other webdrivers
-# currently only Chrome can test for console errors
-# Also tests that segment track call gets sent on click
-class JoinBtnSegmentTestCase(SeleniumTestCase):
+class HIWPageSegmentTestCase(SeleniumTestCase):
 
 
     # basic page call test
-    def test_solutionCenterPageLoadSegment(self):
+    def test_hIWSegmentPagecall(self):
         # quit if browser didn't load
         if not self.client:
             return 0
 
         # req'd params: current test case (self), relative path to triggering page, target element, and the triggering action
-        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/articles/?CacheBuster')
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/basic/how-it-works.htm?bustARhyme')
 
         # describe what you are expecting in the segment call.
         # main field is how Python finds the test data in the page info, should probably be 'name' or 'activityLocation'
@@ -24,16 +19,15 @@ class JoinBtnSegmentTestCase(SeleniumTestCase):
         # Take note of formatting...
         segcall_info = {
             'main_field': 'name',
-            'main_value': 'CNT : Article Landing Page',
+            'main_value': 'Visitor : How It Works',
             'segment_params': [
-                ('atTestOffer'), #example of what we want to do when only checking for property name
-                ('cid', ''),
-                ('path', '/articles/'),
-                ('title', 'Solution Center - Tips, Advice, and Ideas | Angie\'s List'),
-                ('userId', ''),
+                ('name', 'Visitor : How It Works'),
+                ('path', '/basic/how-it-works.htm'),
+                ('title', 'How Does Angie\'s List Work | Angie\'s List'),
+                ('atTestOffer',),
+                ('userId',),
                 ('userType', 'Visitor - New'),
             ]
         }
 
         SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
-
