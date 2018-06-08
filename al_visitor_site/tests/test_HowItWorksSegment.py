9014c329d6fd7bdb8582e94ab4f08db6c5858610
AaronBe <aaronbe@angieslist.com>
2018-06-08 14:38:53 -0400

    How It Works Segment Round Uno
    Adding all click and page load Segment events.

--- al_visitor_site/tests/test_HowItWorksSegment.py
+++ al_visitor_site/tests/test_HowItWorksSegment.py
@@ -1,17 +1,17 @@
 from tests.SeleniumTestCase import SeleniumTestCase
 from SegmentTestHelper import SegmentTestHelper
 
-class HIWPageSegmentTestCase(SeleniumTestCase):
+class HowItWorksPageSegmentTestCase(SeleniumTestCase):
 
 
     # basic page call test
-    def test_hIWSegmentPagecall(self):
+    def test_howItWorksSegmentPagecall(self):
         # quit if browser didn't load
         if not self.client:
             return 0
 
         # req'd params: current test case (self), relative path to triggering page, target element, and the triggering action
-        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/basic/how-it-works.htm?bustARhyme')
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/how-it-works.htm?bustARhyme')
 
         # describe what you are expecting in the segment call.
         # main field is how Python finds the test data in the page info, should probably be 'name' or 'activityLocation'
@@ -22,11 +22,158 @@ class HIWPageSegmentTestCase(SeleniumTestCase):
             'main_value': 'Visitor : How It Works',
             'segment_params': [
                 ('name', 'Visitor : How It Works'),
-                ('path', '/basic/how-it-works.htm'),
+                ('path', '/how-it-works.htm'),
                 ('title', 'How Does Angie\'s List Work | Angie\'s List'),
                 ('atTestOffer',),
                 ('userId',),
                 ('userType', 'Visitor - New'),
+            ]
+        }
+
+        SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
+
+
+    # test segment call on clicking the join link in the header
+    def test_howItWorksHeaderJoinClick(self):
+        if not self.client:
+            return 0
+
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/how-it-works.htm?bustARhyme', '#header-join', 'click')
+        segcall_info = {
+            'main_field': 'activityLocation',
+            'main_value': 'Visitor : How It Works',
+            'segment_params': [
+                ('description', 'Join link in header'),
+                ('userId',),
+            ]
+        }
+
+        SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
+
+
+
+    # test segment call on clicking the HIW link in the header
+    def test_howItWorksHeaderLinkClick(self):
+        if not self.client:
+            return 0
+
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/how-it-works.htm?bustARhyme', '.btnHiw', 'click')
+        segcall_info = {
+            'main_field': 'activityLocation',
+            'main_value': 'Visitor : How It Works',
+            'segment_params': [
+                ('description', 'How it Works link in header'),
+                ('userId',),
+            ]
+        }
+
+        SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
+
+
+
+
+    # test segment call on clicking the Sign In link in the header
+    def test_howItWorksHeaderSignInClick(self):
+        if not self.client:
+            return 0
+
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/how-it-works.htm?bustARhyme', '#header-sign-in', 'click')
+        segcall_info = {
+            'main_field': 'activityLocation',
+            'main_value': 'Visitor : How It Works',
+            'segment_params': [
+                ('description', 'Sign In link in header'),
+                ('userId',),
+            ]
+        }
+
+        SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
+
+
+    # test segment call on clicking the FAQ link in the header
+    def test_howItWorksHeaderFAQClick(self):
+        if not self.client:
+            return 0
+
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/how-it-works.htm?bustARhyme', '#faq', 'click')
+        segcall_info = {
+            'main_field': 'activityLocation',
+            'main_value': 'Visitor : How It Works',
+            'segment_params': [
+                ('description', 'FAQ link in header'),
+                ('userId',),
+            ]
+        }
+
+        SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
+
+
+    # test segment call on clicking the Press link in the header
+    def test_howItWorksHeaderITPClick(self):
+        if not self.client:
+            return 0
+
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/how-it-works.htm?bustARhyme', '#press', 'click')
+        segcall_info = {
+            'main_field': 'activityLocation',
+            'main_value': 'Visitor : How It Works',
+            'segment_params': [
+                ('description', 'In the Press link in header'),
+                ('userId',),
+            ]
+        }
+
+        SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
+
+
+    # test segment call on clicking the SC link in the header
+    def test_howItWorksHeaderScClick(self):
+        if not self.client:
+            return 0
+
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/how-it-works.htm?bustARhyme', '#articles', 'click')
+        segcall_info = {
+            'main_field': 'activityLocation',
+            'main_value': 'Visitor : How It Works',
+            'segment_params': [
+                ('description', 'Solution Center link in header'),
+                ('userId',),
+            ]
+        }
+
+        SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
+
+
+    # test segment call on clicking the BC link in the header
+    def test_howItWorksHeaderScClick(self):
+        if not self.client:
+            return 0
+
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/how-it-works.htm?bustARhyme', '#nav_business_owner', 'click')
+        segcall_info = {
+            'main_field': 'activityLocation',
+            'main_value': 'Visitor : How It Works',
+            'segment_params': [
+                ('description', 'Business Owners link in header'),
+                ('userId',),
+            ]
+        }
+
+        SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
+
+
+    # test segment call on clicking the Join link in the footer
+    def test_howItWorksFooterJoinClick(self):
+        if not self.client:
+            return 0
+
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/how-it-works.htm?bustARhyme', '#footer-join', 'click')
+        segcall_info = {
+            'main_field': 'activityLocation',
+            'main_value': 'H: How it Works',
+            'segment_params': [
+                ('description', 'Join button in footer'),
+                ('userId',),
             ]
         }
 
