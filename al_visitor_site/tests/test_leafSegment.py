4a9872f83b452bafb9d5f55e7a00ea70e16c884f
Donna <donna.street@angieslist.com>
2018-06-13 11:45:19 -0400

    Leaf Segment
    Added Segment test for Leaf page load

--- al_visitor_site/tests/test_leafSegment.py
+++ al_visitor_site/tests/test_leafSegment.py
@@ -7,6 +7,37 @@ from SegmentTestHelper import SegmentTestHelper
 # Also tests that segment track call gets sent on click
 class LeafPageSegmentTestCase(SeleniumTestCase):
 
+
+    # basic leaf page call test
+    def test_leafPageSegmentPagecall(self):
+        # quit if browser didn't load
+        if not self.client:
+            return 0
+
+        # req'd params: current test case (self), relative path to triggering page, target element, and the triggering action
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/?CacheBuster')
+
+        # describe what you are expecting in the segment call.
+        # main field is how Python finds the test data in the page info, should probably be 'name' or 'activityLocation'
+        # segment_params is a list of other segment fields to check. Can include main field as well.
+        # Take note of formatting...
+        segcall_info = {
+            'main_field': 'name',
+            'main_value': 'Visitor : SP Profile',
+            'segment_params': [
+                ('name', 'Visitor : SP Profile'),
+                ('title', 'Angie\'s List | Join for FREE to see 10 Million Verified Reviews'),
+                ('srCtaDisplayed', True),
+                ('srCtaVersion', 'v2.1'),
+                ('userId',),
+                ('userType', 'Visitor - New'),
+            ]
+        }
+
+        SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
+
+
+    # SR ha-lead-submit-v2 w/o postal code
     def skiptest_leafPageSegmentCTAwithoutZIP(self):
         if not self.client:
             return 0
@@ -23,7 +54,7 @@ class LeafPageSegmentTestCase(SeleniumTestCase):
 
         SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
 
-
+    # SR ha-lead-submit-v2 with postal code
     def test_leafPageSegmentCTAwithZIP(self):
         if not self.client:
             return 0
@@ -47,3 +78,46 @@ class LeafPageSegmentTestCase(SeleniumTestCase):
         }
 
         SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
+
+
+    # Footer Join
+    def test_footerJoinCTA(self):
+        # quit if browser didn't load
+        if not self.client:
+            return 0
+
+        # req'd params: current test case (self), relative path to triggering page, target element, and the triggering action
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/', '#footer-join', 'click')
+
+        segcall_info = {
+            'main_field': 'activityLocation',
+            'main_value': 'Visitor : SP Profile',
+            'segment_params': [
+                ('description', 'Join button in footer'),
+                ('userId', ''),
+            ]
+        }
+
+        SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
+
+
+    # Header Join
+    def test_headerJoinCTA(self):
+        # quit if browser didn't load
+        if not self.client:
+            return 0
+
+        # req'd params: current test case (self), relative path to triggering page, target element, and the triggering action
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/', '#header-join', 'click')
+
+        segcall_info = {
+            'main_field': 'activityLocation',
+            'main_value': 'Visitor : SP Profile',
+            'segment_params': [
+                ('description', 'Join link in header'),
+                ('userId', ''),
+            ]
+        }
+
+        SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
+
