9bb6141f9867356926dea8dac53fbca23c760b4a
AaronBe <aaronbe@angieslist.com>
2018-06-15 12:40:09 -0400

    combining tests
    rum + jquery check

--- al_visitor_site/tests/test_sourceTests.py
+++ al_visitor_site/tests/test_sourceTests.py
@@ -6,83 +6,85 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
+
+class CheckingSourceTestCase(SeleniumTestCase):
+
+    def test_pageSource(self):
+        if self.client:
+            test_urls = [ "/",
+                          "/articles/",
+                          "/home-improvement/",
+                          "/auto/",
+                          "/videos/",
+                          "/how-it-works.htm",
+                          "/hvac/",
+                          "/faq/",
+                          "/photos/",
+                          "/videos/",
+                          "/in-the-press.htm",
+                          "/author/angie-hicks.htm",
+                          "/companylist/",
+                          "/companylist/us/in/",
+                          "/companylist/us/in/greenwood/a-new-horizon-renovations-llp-reviews-6422533.htm",
+            ]
+            
+            # start_time = "START TIME: " + str(datetime.datetime.now())
+            error_count = 0
+            errors = ""
+            success_count = 0
+            wait = WebDriverWait(self.client, 2)
+            varnish_buster = "?asdf7654"
+            for test_url in test_urls:
+                self.client.get(self.visitor_site_url + test_url + varnish_buster)
+                wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div')))
+
+
+                self.assertTrue(self.isElementPresent('script[src="http://visitordev.angieslist.com/sites/all/modules/jquery_update/replace/jquery/1.7/jquery.min.js?v=1.7.1"]'))
+                self.assertTrue(self.isElementPresent('script[src="/sites/all/libraries/rum/rum.min.js"]'))
+
+
+
+                #jV = self.client.find_elements_by_css_selector('script[src="http://visitordev.angieslist.com/misc/jquery.once.js?v=1.2"]')
+                #self.assertTrue(self.isElementPresent('script[src="http://visitordev.angieslist.com/sites/all/modules/jquery_update/replace/jquery/1.7/jquery.min.js?v=1.7.1"]'))
+                #self.assertTrue(len(jV))
+
+
+
 ##
 # Check all representative pages in regression template for existence of RUM
 ##
-class RumTestCase(SeleniumTestCase):
+# class RumTestCase(SeleniumTestCase):
 
-    def test_rum(self):
-        if self.client:
-            test_urls = [ "/",
-                          "/articles/",
-                          "/home-improvement/",
-                          "/auto/",
-                          "/videos/",
-                          "/how-it-works.htm",
-                          "/hvac/",
-                          "/faq/",
-                          "/photos/",
-                          "/videos/",
-                          "/in-the-press.htm",
-                          "/author/angie-hicks.htm",
-                          "/companylist/",
-                          "/companylist/us/in/",
-                          "/companylist/us/in/greenwood/a-new-horizon-renovations-llp-reviews-6422533.htm",
-            ]
+#     def test_rum(self):
+#         if self.client:
+#             test_urls = [ "/",
+#                           "/articles/",
+#                           "/home-improvement/",
+#                           "/auto/",
+#                           "/videos/",
+#                           "/how-it-works.htm",
+#                           "/hvac/",
+#                           "/faq/",
+#                           "/photos/",
+#                           "/videos/",
+#                           "/in-the-press.htm",
+#                           "/author/angie-hicks.htm",
+#                           "/companylist/",
+#                           "/companylist/us/in/",
+#                           "/companylist/us/in/greenwood/a-new-horizon-renovations-llp-reviews-6422533.htm",
+#             ]
             
-            # start_time = "START TIME: " + str(datetime.datetime.now())
-            error_count = 0
-            errors = ""
-            success_count = 0
-            wait = WebDriverWait(self.client, 2)
-            varnish_buster = "?asdf7654"
-            for test_url in test_urls:
-                self.client.get(self.visitor_site_url + test_url + varnish_buster)
-                wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div')))
+#             # start_time = "START TIME: " + str(datetime.datetime.now())
+#             error_count = 0
+#             errors = ""
+#             success_count = 0
+#             wait = WebDriverWait(self.client, 2)
+#             varnish_buster = "?asdf7654"
+#             for test_url in test_urls:
+#                 self.client.get(self.visitor_site_url + test_url + varnish_buster)
+#                 wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div')))
 
 
 
-                rum = self.client.find_elements_by_css_selector('script[src="/sites/all/libraries/rum/rum.min.js"]')
-                self.assertTrue(len(rum))
-
-
-
-
-class JVTestCase(SeleniumTestCase):
-
-    def test_jV(self):
-        if self.client:
-            test_urls = [ "/",
-                          "/articles/",
-                          "/home-improvement/",
-                          "/auto/",
-                          "/videos/",
-                          "/how-it-works.htm",
-                          "/hvac/",
-                          "/faq/",
-                          "/photos/",
-                          "/videos/",
-                          "/in-the-press.htm",
-                          "/author/angie-hicks.htm",
-                          "/companylist/",
-                          "/companylist/us/in/",
-                          "/companylist/us/in/greenwood/a-new-horizon-renovations-llp-reviews-6422533.htm",
-            ]
-            
-            # start_time = "START TIME: " + str(datetime.datetime.now())
-            error_count = 0
-            errors = ""
-            success_count = 0
-            wait = WebDriverWait(self.client, 2)
-            varnish_buster = "?asdf7654"
-            for test_url in test_urls:
-                self.client.get(self.visitor_site_url + test_url + varnish_buster)
-                wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div')))
-
-
-
-                jV = self.client.find_elements_by_css_selector('script[src="http://visitordev.angieslist.com/misc/jquery.once.js?v=1.2"]')
-                self.assertTrue(len(jV))
-
-
-
+#                 rum = self.client.find_elements_by_css_selector('script[src="/sites/all/libraries/rum/rum.min.js"]')
+#                 self.assertTrue(len(rum))
