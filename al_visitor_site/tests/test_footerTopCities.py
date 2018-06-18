7d235cb679d006ed698da33c2c3c6c635e118de4
AaronBe <aaronbe@angieslist.com>
2018-06-18 11:35:58 -0400

    Cleaning up
    removing comment

--- al_visitor_site/tests/test_footerTopCities.py
+++ al_visitor_site/tests/test_footerTopCities.py
@@ -82,19 +82,3 @@ class FooterTopCitiesTestCase(SeleniumTestCase):
                 # Possibly REMOVE
                 #leaf = self.client.find_elements_by_css_selector('div.footer-region.cities-left > li > a[title="Search New York Pros"]')
                 #self.assertTrue(len(leaf))
-
-
-           # Possibly REMOVE
-           # # start_time = "START TIME: " + str(datetime.datetime.now())
-           #  error_count = 0
-           #  errors = ""
-           #  success_count = 0
-           #  wait = WebDriverWait(self.client, 2)
-           #  varnish_buster = "?asdf7654"
-           #  for test_url in test_urls:
-           #      self.client.get(self.visitor_site_url + test_url + varnish_buster)
-           #      wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div')))
-
-
-           #      self.assertTrue(self.isElementPresent('script[src="https://www.angieslist.com/sites/all/modules/jquery_update/replace/jquery/1.7/jquery.min.js?v=1.7.1"]'.replace('https://www.angieslist.com', self.visitor_site_url)))
-           #      self.assertTrue(self.isElementPresent('script[src="/sites/all/libraries/rum/rum.min.js"]'))
