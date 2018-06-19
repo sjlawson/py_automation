0253815e7aa7038c3cd501014728886f54436280
AaronBe <aaronbe@angieslist.com>
2018-06-19 11:36:16 -0400

    Pushing for Samuel to see

--- al_visitor_site/tests/test_companyListSegment.py
+++ al_visitor_site/tests/test_companyListSegment.py
@@ -1,2 +1,21 @@
 from tests.SeleniumTestCase import SeleniumTestCase
 from SegmentTestHelper import SegmentTestHelper
+
+# /companylis/ section
+class CompanyListTestCase(SeleniumTestCase):
+
+    # test segment call on clicking the join link in home page header
+    def test_companyListSegmentJoin(self):
+        if not self.client:
+            return 0
+
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/companylist/', '#header-join', 'click')
+        segcall_info = {
+            'main_field': 'activityLocation',
+            'main_value': 'jankSwank : Home',
+            'segment_params': [
+                ('activityLocation', 'jankSwank'),
+            ]
+        }
+
+        SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
