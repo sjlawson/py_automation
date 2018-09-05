43bd64b8121133d6b8ea6e3c4cc360c50d5faa9f
AaronBe <aaronbe@angieslist.com>
2018-09-05 12:38:41 -0400

    pushin change for nathan

--- al_visitor_site/tests/test_test123.py
+++ al_visitor_site/tests/test_test123.py
@@ -1,7 +1,43 @@
 from tests.SeleniumTestCase import SeleniumTestCase
 from SegmentTestHelper import SegmentTestHelper
 
-class visitorReviewWorkCompleted(SeleniumTestCase):
+
+##### Company List Page ####
+class TestCompanyListTestCase(SeleniumTestCase):
+
+
+## Header Segment Events - Company List ##
+
+    # test segment call on clicking the join link in company list header
+    def test_testCompanyListSegmentJoin(self):
+        if not self.client:
+            return 0
+
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/companylist/?bustA1', '#header-join', 'click')
+        segcall_info = {
+            'main_field': 'activityLocation',
+            'main_value': 'Visitor : Directory',
+            'segment_params': [
+                ('activityLocation', 'Visitor : Directory'),
+                ('description', 'Join link in header'),
+                ('userId',),
+            ]
+        }
+
+        SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
+
+
+
+
+
+
+
+
+
+
+
+
+""" class visitorReviewWorkCompleted(SeleniumTestCase):
 
     # basic leaf page call test for advertisers
     def test_vrfWorkCompletedYes(self):
@@ -21,4 +57,4 @@ class visitorReviewWorkCompleted(SeleniumTestCase):
             ]
         }
 
-        SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
+        SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info) """
