a8a7621dc71916264debc709043b97458855061d
Nathan Barnes <nathanb@angieslist.com>
2018-09-05 12:58:12 -0400

    test

--- al_visitor_site/tests/test_test123.py
+++ al_visitor_site/tests/test_test123.py
@@ -13,14 +13,13 @@ class TestCompanyListTestCase(SeleniumTestCase):
         if not self.client:
             return 0
 
-        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/companylist/?bustA1', '#header-join', 'click')
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/edit?serviceProviderId=10803144', '#reviews—workComplete-button-yes', 'click')
         segcall_info = {
-            'main_field': 'activityLocation',
-            'main_value': 'Visitor : Directory',
+            'main_field': 'reviewSection',
+            'main_value': 'Work Done',
             'segment_params': [
-                ('activityLocation', 'Visitor : Directory'),
-                ('description', 'Join link in header'),
-                ('userId',),
+                ('reviewSection', 'Work Done'),
+                
             ]
         }
 
@@ -37,24 +36,3 @@ class TestCompanyListTestCase(SeleniumTestCase):
 
 
 
-""" class visitorReviewWorkCompleted(SeleniumTestCase):
-
-    # basic leaf page call test for advertisers
-    def test_vrfWorkCompletedYes(self):
-        # quit if browser didn't load
-        if not self.client:
-            return 0
-
-
-    collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/edit?serviceProviderId=10803144', '#reviews—workComplete-button-yes', 'click')
-        
-
-    segcall_info = {
-            'main_field': 'reviewSection',
-            'main_value': 'Work Done',
-            'segment_params': [
-                ('reviewSection', 'Work Done'),
-            ]
-        }
-
-        SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info) """
