8d572ba100876f731930a608e2bbb0e48075adcf
AaronBe <aaronbe@angieslist.com>
2018-09-21 08:26:05 -0400

    Header events.

--- al_visitor_site/tests/test_nearMeThunderball.py
+++ al_visitor_site/tests/test_nearMeThunderball.py
@@ -4,7 +4,7 @@ from SegmentTestHelper import SegmentTestHelper
 class NearMeThunderballPageSegmentTestCase(SeleniumTestCase):
 
     # basic page call test
-    def test_nearMeThunderballSegmentPagecall(self):
+    def test_nearMePlumbingThunderballSegmentPagecall(self):
         # quit if browser didn't load
         if not self.client:
             return 0
@@ -24,6 +24,150 @@ class NearMeThunderballPageSegmentTestCase(SeleniumTestCase):
                 ('referrer',),
                 ('search',),
                 ('url',),
+            ]
+        }
+
+        SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
+
+    #  AL Header link on NearMe - Tball
+    def test_nearMePlumbingHeaderAlLinkTball(self):
+        if not self.client:
+            return 0
+
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/nearme/plumbing/', '#header--al-logo-link', 'click')
+
+        segcall_info = {
+            'main_field': 'description',
+            'main_value': 'Angie’s List logo in header',
+            'segment_params': [
+                ('description', 'Angie’s List logo in header'),
+                # ('activityLocation', 'Visitor : Near Me')
+            ]
+        }
+
+        SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
+
+    # Near Me join link in the header
+    def test_nearMePlumbingTballHeaderJoinClick(self):
+        if not self.client:
+            return 0
+
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/nearme/plumbing/', '#header--join-for-free-button', 'click')
+        segcall_info = {
+            'main_field': 'description',
+            'main_value': 'Join link in header',
+            'segment_params': [
+                ('description', 'Join link in header'),
+                # ('activityLocation', 'Visitor : Near Me'),
+            ]
+        }
+
+        SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
+
+    # Near Me link in the header
+    def test_nearMePlumbingTballHeaderLinkClick(self):
+        if not self.client:
+            return 0
+
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/nearme/plumbing/', '#header--Near Me-link', 'click')
+        segcall_info = {
+            'main_field': 'description',
+            'main_value': 'How It Works link in header',
+            'segment_params': [
+                ('description', 'How It Works link in header'),
+                # ('activityLocation', 'Visitor : Near Me'),
+            ]
+        }
+
+        SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
+
+    # Near Me Sign In link in the header
+    def test_nearMePlumbingTballSignInLinkClick(self):
+        if not self.client:
+            return 0
+
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/nearme/plumbing/', '#header--sign-in-link', 'click')
+        segcall_info = {
+            'main_field': 'description',
+            'main_value': 'Sign In link in header',
+            'segment_params': [
+                ('description', 'Sign In link in header'),
+                # ('activityLocation', 'Visitor : Near Me'),
+            ]
+        }
+
+        SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
+
+
+    #  Near Me FAQ link in header Tball
+    def test_nearMePlumbingHeaderFAQLinkTball(self):
+        if not self.client:
+            return 0
+
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/nearme/plumbing/', '#header--faq-link', 'click')
+
+        segcall_info = {
+            'main_field': 'description',
+            'main_value': 'FAQ link in header',
+            'segment_params': [
+                ('description', 'FAQ link in header'),
+                # ('activityLocation', 'Visitor : Near Me')
+            ]
+        }
+
+        SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
+
+
+    #  Near Me In The Press link in header Tball
+    def test_nearMePlumbingHeaderInThePressLinkTball(self):
+        if not self.client:
+            return 0
+
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/nearme/plumbing/', '#header--in-the-press-link', 'click')
+
+        segcall_info = {
+            'main_field': 'description',
+            'main_value': 'In the Press link in header',
+            'segment_params': [
+                ('description', 'In the Press link in header'),
+                # ('activityLocation', 'Visitor : Near Me')
+            ]
+        }
+
+        SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
+
+
+    #  Near Me Solution Center link in header Tball
+    def test_nearMePlumbingHeaderSolutionCenterLinkTball(self):
+        if not self.client:
+            return 0
+
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/nearme/plumbing/', '#header--articles-link', 'click')
+
+        segcall_info = {
+            'main_field': 'description',
+            'main_value': 'Solution Center link in header',
+            'segment_params': [
+                ('description', 'Solution Center link in header'),
+                # ('activityLocation', 'Visitor : Near Me')
+            ]
+        }
+
+        SegmentTestHelper.do_segment_assertions(self, collect_seg_calls, segcall_info)
+
+    #  Near Me Business Owners link in header Tball
+    def test_nearMePlumbingHeaderBusinessOwnerLinkTball(self):
+        if not self.client:
+            return 0
+
+        collect_seg_calls = SegmentTestHelper.gather_segment_requests_for_url(self, '/nearme/plumbing/', '#header--business-owners-link', 'click')
+
+        segcall_info = {
+            'main_field': 'description',
+            'main_value': 'Business Owners link in header',
+            'segment_params': [
+                ('description', 'Business Owners link in header'),
+                # ('activityLocation', 'Visitor : Near Me')
             ]
         }
 
