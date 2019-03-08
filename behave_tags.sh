959a2938602a4f66b31f9532a47f58bb0d914e88
Samuel J. Lawson <samuel.lawson@angieslist.com>
2019-03-08 14:58:19 -0500

    quack12: restored post-test xml path

--- behave_tags.sh
+++ behave_tags.sh
@@ -2,4 +2,4 @@
 
 echo "Running cmd: export TESTENV=$1; export BROWSERNAME=$2; behave --tags=$3 -k"
 export TESTENV=$1; export BROWSERNAME=$2; behave --tags=$3 -k
-mv reports/test-results.xml .
+# mv reports/test-results.xml .
