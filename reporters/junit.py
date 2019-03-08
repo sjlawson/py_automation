f4853e9fae84b581edcaaa3517f15262c07492a1
Samuel J. Lawson <samuel.lawson@angieslist.com>
2019-03-08 14:13:50 -0500

    quack12: product and move xml result

--- reporters/junit.py
+++ reporters/junit.py
@@ -268,9 +268,10 @@ class JUnitReporter(Reporter):
 
         tree = ElementTreeWithCDATA(suite)
         report_dirname = self.config.junit_directory
-        report_basename = u'TESTS-%s.xml' % (feature_filename + '_' + datetime.now().strftime("%Y%m%d%H%m%S"))
+        report_basename = u'test-results.xml'
+        # u'TESTS-%s.xml' % (feature_filename + '_' + datetime.now().strftime("%Y%m%d%H%m%S"))
         report_filename = os.path.join(report_dirname, report_basename)
-        tree.write(codecs.open(report_filename, "wb"), "UTF-8")
+        tree.write(codecs.open(report_filename, "ab"), "UTF-8")
 
     # -- MORE:
     # pylint: disable=line-too-long
