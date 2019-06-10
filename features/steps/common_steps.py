a444b37d70b459ca7a76a7db2d5243c40a060d02
Samuel J. Lawson <samuel.lawson@angieslist.com>
2019-06-10 17:10:09 -0400

    QUACK-86: added helper for page load. Implemented pep8 linting

--- features/steps/common_steps.py
+++ features/steps/common_steps.py
@@ -2,17 +2,20 @@ from behave import given, then, when
 from common.SegmentTestHelper import SegmentTestHelper
 from common.BehaveStepHelper import BehaveStepHelper
 from selenium.webdriver.common.keys import Keys
-import time
+
 
 @then('press the "{keycode}" key while "{element}" is in focus')
 def step_impl(context, keycode, element):
-    """ Key codes are listed at: https://selenium-python.readthedocs.io/api.html#module-selenium.webdriver.common.keys"""
+    """ Key codes are listed at:
+    https://selenium-python.readthedocs.io/api.html#module-selenium.webdriver.common.keys"""
     element = SegmentTestHelper.get_webdriver_element(context.browser, element, 0)
     element.send_keys(getattr(Keys, keycode))
 
+
 @when('the page has finished loading')
 def step_impl(context):
-    time.sleep(3)
+    BehaveStepHelper.page_loaded(context)
+
 
 @then('the page should meet "{acc_standard}" accessibility guidelines')
 def step_impl(context, acc_standard):
