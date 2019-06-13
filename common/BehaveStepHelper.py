faa2240dd1baf7bf4682cd072e4ce8b11efb9520
Samuel J. Lawson <samuel.lawson@angieslist.com>
2019-06-13 09:52:37 -0400

    QUACK-84: merged path from QUACK-86

--- common/BehaveStepHelper.py
+++ common/BehaveStepHelper.py
@@ -1,11 +1,14 @@
 from axe_selenium_python import Axe
+import time
+
 
 class BehaveStepHelper:
 
-    def accessibility(context):
+    def accessibility(context, acc_standard):
         axe = Axe(context.browser)
         axe.inject()
-        results = axe.run(options={'runOnly': { 'type': 'tag', 'values': ['wcag2a']}})
+        results = axe.run(
+            options={'runOnly': {'type': 'tag', 'values': [acc_standard]}})
         current_url = context.browser.current_url
         try:
             assert len(results["violations"]) == 0
@@ -13,11 +16,11 @@ class BehaveStepHelper:
         except AssertionError as ae:
             context.test_case.test_result = 'fail'
             violations = str(axe.report(results["violations"]))
-            message = 'Page at url: %s failed accesibility with: %s ' % (current_url, violations)
+            message = 'Page at url: %s failed accesibility with: %s ' %\
+                (current_url, violations)
             with open('./reports/accessibility_violations.txt', 'a') as report:
                 report.write(message)
             raise AssertionError(message)
-
 
     def page_loaded(context):
         time.sleep(3)
