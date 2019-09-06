8e7b78b8e22539d2fd0d199df4d95133863b683e
AaronBe <aaronbe@homeadvisor.com>
2019-09-06 11:45:03 -0400

    REFACTOR

--- features/steps/reviewFormTests.py
+++ features/steps/reviewFormTests.py
@@ -1,41 +1,5 @@
 from selenium.webdriver.common.by import By
 
-class MemberJoinVRF(object):
-  JOIN_EMAIL = (By.ID, 'signup-form--email')
-  JOIN_FIRST_NAME = (By.ID, 'signup-form--first-name')
-  JOIN_LAST_NAME = (By.ID, 'signup-form--last-name')
-  JOIN_PASSWORD = (By.ID, 'signup-form--password')
-  JOIN_TAB = (By.ID, 'sign-up-toggle')
-
-class VRFJoinAddressLocators(object):
-  ADDRESS_FIRST_LINE = (By.ID, 'address-component--first-line')
-  ADDRESS_SECOND_LINE = (By.ID, 'address-component--second-line')
-  ADDRESS_CITY = (By.ID, 'address-component--city')
-  ADDRESS_STATE = (By.ID, 'address-component--state')
-  ADDRESS_ZIP = (By.ID, 'address-component--zip-code')
-  ADDRESS_SUBMIT = (By.ID, 'address-collection--submit-button')
-
-class VRFFinishButtons(object):
-  SUBMIT_BUTTON = (By.ID, 'signup-form--submit')
-  USER_AGREEMENT_BUTTON = (By.ID, 'signup-form--membership-agreement')
-
-class VRFHappyPathSelectors(object):
-  OVERALL_A_GRADE = (By.ID, 'reviews--rating-Overall--A')
-  PRICE_A_GRADE = (By.ID, 'reviews--rating-Price--A')
-  QUALITY_A_GRADE = (By.ID, 'reviews--rating-Quality--A')
-  RESPONSIVENESS_A_GRADE = (By.ID, 'reviews--rating-Responsiveness--A')
-  PUNCTUALITY_A_GRADE = (By.ID, 'reviews--rating-Punctuality--A')
-  PROFESSIONALISM_A_GRADE = (By.ID, 'reviews--rating-Professionalism--A')
-  WORK_COMPLETED_YES = (By.ID, 'reviews--workComplete-button-yes')
-  WILL_HIRE_AGAIN_YES = (By.ID, 'reviews--willHireAgain-button-yes')
-  DESCRIPTION_OF_WORK = (By.ID, 'review--description-of-work-input')
-  HOW_DID_IT_GO = (By.ID, 'review--how-did-it-go-input')
-  REVIEW_SUBMIT_BUTTON = (By.ID, 'reviews--submit-button')
-  SIGN_IN_TOGGLE = (By.ID, 'sign-in-toggle')
-  SIGN_IN_EMAIL = (By.ID, 'signin-form--email')
-  SIGN_IN_PASSWORD = (By.ID, 'signin-form--password')
-  SING_IN_FORM_SUBMIT = (By.ID, 'signin-form--submit')
-
 class ReviewLetterGradeSelectors(object):
   AVAILABILITY_A_GRADE = (By.ID, 'reviews--rating-Availability--A')
   AVAILABILITY_B_GRADE = (By.ID, 'reviews--rating-Availability--B')
@@ -115,15 +79,35 @@ class ReviewLetterGradeSelectors(object):
   STAFF_FRIENDLINESS_F_GRADE = (By.ID, 'reviews--rating-Staff Friendliness--F')
   STAFF_FRIENDLINESS_NA_GRADE = (By.ID, 'reviews--rating-Staff Friendliness--N/A')
 
-class VRFHappyMixedPathSelectors(object):
-  OVERALL_B_GRADE = (By.ID, 'reviews--rating-Overall--B')
-  PRICE_A_GRADE = (By.ID, 'reviews--rating-Price--A')
-  QUALITY_C_GRADE = (By.ID, 'reviews--rating-Quality--C')
-  RESPONSIVENESS_A_GRADE = (By.ID, 'reviews--rating-Responsiveness--A')
-  PUNCTUALITY_B_GRADE = (By.ID, 'reviews--rating-Punctuality--B')
-  PROFESSIONALISM_A_GRADE = (By.ID, 'reviews--rating-Professionalism--A')
+class ReviewButtons(object):
   WORK_COMPLETED_YES = (By.ID, 'reviews--workComplete-button-yes')
+  WORK_COMPLETED_NO = (By.ID, 'reviews--workComplete-button-no')
+  WILL_HIRE_AGAIN_YES = (By.ID, 'reviews--willHireAgain-button-yes')
   WILL_HIRE_AGAIN_NO = (By.ID, 'reviews--willHireAgain-button-no')
+  MEDIA_BUTTON = (By.ID, 'reviews--add-media-button')
+  SIGN_IN_FORM_SUBMIT = (By.ID, 'signin-form--submit')
+  SIGN_IN_TOGGLE = (By.ID, 'sign-in-toggle')
+  SIGN_UP_TOGGLE = (By.ID, 'sign-up-toggle')
+  USER_AGREEMENT_BUTTON = (By.ID, 'signup-form--membership-agreement')
+  COMMUNICATION_OPT_IN = (By.ID, 'signup-form--email-opt-in')
+
+class ReviewInput(object):
+  CALENDAR_INPUT = (By.ID, 'calendar-input-input')
+  CATEGORY_INPUT = (By.ID, 'write-a-review--category-auto-complete-input-input')
+  COST_OF_SERVICE_INPUT = (By.ID, 'review--cost-of-service-input')
   DESCRIPTION_OF_WORK = (By.ID, 'review--description-of-work-input')
   HOW_DID_IT_GO = (By.ID, 'review--how-did-it-go-input')
   REVIEW_SUBMIT_BUTTON = (By.ID, 'reviews--submit-button')
+  SIGN_IN_EMAIL = (By.ID, 'signin-form--email')
+  SIGN_IN_PASSWORD = (By.ID, 'signin-form--password')
+  SIGN_UP_FORM_EMAIL = (By.ID, 'signup-form--email')
+  SIGN_UP_FIRST_NAME = (By.ID, 'signup-form--first-name')
+  SIGN_UP_LAST_NAME = (By.ID, 'signup-form--last-name')
+  SIGN_UP_PASSWORD = (By.ID, 'signup-form--password')
+  ADDRESS_FIRST_LINE = (By.ID, 'address-component--first-line')
+  ADDRESS_SECOND_LINE = (By.ID, 'address-component--second-line')
+  ADDRESS_CITY = (By.ID, 'address-component--city')
+  ADDRESS_STATE = (By.ID, 'address-component--state')
+  ADDRESS_ZIP = (By.ID, 'address-component--zip-code')
+  ADDRESS_SUBMIT = (By.ID, 'address-collection--submit-button')
+
