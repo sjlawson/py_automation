caa1e49fb4a501fa867b63fdacea13000c06ea12
Donna <donna.street@angieslist.fcom>
2019-10-01 12:04:05 -0400

    HomePage Updates

--- features/steps/visitorsite_steps.py
+++ features/steps/visitorsite_steps.py
@@ -147,6 +147,8 @@ def step_impl(context):
     context.browser.get(context.url)
     time.sleep(1)
 
+
+
 @given('user is on a visitor site leaf non advertiser page')
 def step_impl(context):
     appsuite_env = 'al_visitor_site'
@@ -154,6 +156,24 @@ def step_impl(context):
     context.url = appsuite_url + '/companylist/us/ga/cartersville/1-oak-roofing-reviews-9205981.htm'
     context.browser.get(context.url)
     time.sleep(1)
+
+@then('user is on a visitor site page')
+def step_impl(context):
+    appsuite_env = 'al_visitor_site'
+    appsuite_url = context.appsuites[appsuite_env]['base_url']
+    context.url = appsuite_url + context.text
+    context.browser.get(context.url)
+    time.sleep(1)
+
+
+@then('user returns to the visitor site page')
+def step_impl(context):
+    appsuite_env = 'al_visitor_site'
+    appsuite_url = context.appsuites[appsuite_env]['base_url']
+    context.url = appsuite_url + context.text
+    context.browser.get(context.url)
+    time.sleep(1)
+
 
 @then('the title of link with id "{link_id}" is "{linktitle}"')
 def step_impl(context, link_id, linktitle):
